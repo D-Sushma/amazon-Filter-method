2ea095a9129b74ead63b6b655ba1e0b8be881ab9
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2022-07-13 21:12:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // USE FILTERATION=================================================================================================
 //. task--> subheader.js me Shirt ko click krne pr only Shirt & Pant kO click krne pr only pant ana chahiye
-//.----->>>so need to add "type" property, taki ham copmo ko filter kr paye------------------------
+//.----->>>so need to add "type" property in Object.js, taki ham copmo ko filter kr paye------------------------
 // 1. go Onbject.js & add type (ex- type:"shirt", pant:"pant")
 // 2. need to import object.js In shirt & pant compo--> so that we get shirt/pant obj frm object.js
 // 3. add filteration fun  with object.js in shirt.js & pant.js compo
-// 4. filter lgane ke bad hme object render krna hai us filter var k sath
+// 4. filter lgane ke bad hme object render krna hai us filter variable k sath
 
 import './App.css';
 import Header from './components/Header'
